22212ac9ffaebbbb3b51164523aa73177d75296e
Samet Senturk <samet.senturk@antalya.detur.local>
2020-08-12 14:45:07 +0300

    commit

---
+++ ui.js
@@ -0,0 +1,20 @@
+class UI{
+    constructor(firstSelect,secondSelect){
+        this.firsSelect = firstSelect;
+        this.secondSelect = secondSelect;
+
+        this.outputFirst = document.getElementById("outputFirst");
+        this.outputSecond = document.getElementById("outputSecond");
+        this.outputResult = document.getElementById("outputResult");
+    }
+
+    changeFirst(){
+        this.outputFirst.textContent = this.firstSelect.options[firstSelect.selectedIndex].textContent;
+    }
+    changeSecond(){
+        this.outputSecond.textContent = this.secondSelect.options[firstSelect.selectedIndex].textContent;
+    }
+    displayResult(result){
+        this.outputResult.value = result;
+    }
+}
